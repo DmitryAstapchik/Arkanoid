95a8cdce93ad43c4e21ad2b97fd66c71881fada8
Dmitriy Astapchik <dim_ast@mail.ru>
2019-06-04 19:53:47 +0300

    Add files via upload

---
+++ Arkanoid/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelPause = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCount.Location = new System.Drawing.Point(66, 22);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(120, 25);
+            this.labelCount.TabIndex = 0;
+            this.labelCount.Text = "Отбиваний:";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.Location = new System.Drawing.Point(66, 59);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(75, 25);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "Очков:";
+            // 
+            // labelStart
+            // 
+            this.labelStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStart.Location = new System.Drawing.Point(88, 235);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(638, 31);
+            this.labelStart.TabIndex = 2;
+            this.labelStart.Text = "Нажмите пробел для начала игры или для паузы.";
+            // 
+            // labelPause
+            // 
+            this.labelPause.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelPause.AutoSize = true;
+            this.labelPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPause.Location = new System.Drawing.Point(289, 221);
+            this.labelPause.Name = "labelPause";
+            this.labelPause.Size = new System.Drawing.Size(207, 63);
+            this.labelPause.TabIndex = 3;
+            this.labelPause.Text = "ПАУЗА";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 509);
+            this.Controls.Add(this.labelPause);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelCount);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Arkanoid";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelPause;
+    }
+}
+
---
+++ Arkanoid/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        Image ball = Image.FromFile("../../images/ball.png");
+        Point ballPoint;
+        Point vesselPoint;
+        int speedX;
+        int speedY = 7;
+        int count = 0;
+        int score = 0;
+        bool paused = true;
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ballPoint = new Point(rand.Next(ClientSize.Width - 100), -100);
+            vesselPoint.X = ClientSize.Width / 2 - 150;
+            vesselPoint.Y = ClientSize.Height - 50;
+            speedX = rand.Next(6, 7) == 6 ? -7 : 7;
+            labelPause.Visible = false;
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(ball, ballPoint.X, ballPoint.Y, 100, 100);
+            e.Graphics.DrawRectangle(new Pen(Color.Black, 5), vesselPoint.X, vesselPoint.Y, 300, 50);
+            e.Graphics.FillRectangle(Brushes.Red, vesselPoint.X, vesselPoint.Y, 300, 50);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && !paused)
+            {
+                vesselPoint.X -= 20;
+            }
+            else if (e.KeyCode == Keys.Right && !paused)
+            {
+                vesselPoint.X += 20;
+            }
+            if (e.KeyCode == Keys.Space && paused)
+            {
+                timer1.Start();
+                paused = false;
+                labelPause.Visible = false;
+                labelStart.Visible = false;
+                Cursor.Hide();
+            }
+            else if (e.KeyCode == Keys.Space && !paused)
+            {
+                timer1.Stop();
+                paused = true;
+                labelPause.Visible = true;
+                Cursor.Show();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (ballPoint.Y + 100 >= vesselPoint.Y)
+            {
+                if (ballPoint.X + 50 >= vesselPoint.X + 120 && ballPoint.X + 50 <= vesselPoint.X + 180)
+                {
+                    speedX = 5 * Math.Sign(speedX);
+                    speedY = -Math.Abs(speedY);
+                    count++;
+                    score += 5;
+                }
+                else if (ballPoint.X + 50 >= vesselPoint.X + 90 && ballPoint.X + 50 <= vesselPoint.X + 210)
+                {
+                    speedX = 6 * Math.Sign(speedX);
+                    speedY = -Math.Abs(speedY);
+                    count++;
+                    score += 4;
+                }
+                else if (ballPoint.X + 50 >= vesselPoint.X + 60 && ballPoint.X + 50 <= vesselPoint.X + 240)
+                {
+                    speedX = 7 * Math.Sign(speedX);
+                    speedY = -Math.Abs(speedY);
+                    count++;
+                    score += 3;
+                }
+                else if (ballPoint.X + 50 >= vesselPoint.X + 30 && ballPoint.X + 50 <= vesselPoint.X + 270)
+                {
+                    speedX = 8 * Math.Sign(speedX);
+                    speedY = -Math.Abs(speedY);
+                    count++;
+                    score += 2;
+                }
+                else if (ballPoint.X + 50 >= vesselPoint.X && ballPoint.X + 50 <= vesselPoint.X + 300)
+                {
+                    speedX = 9 * Math.Sign(speedX);
+                    speedY = -Math.Abs(speedY);
+                    count++;
+                    score += 1;
+                }
+            }
+
+            if (ballPoint.Y < 0)
+            {
+                speedY = Math.Abs(speedY);
+            }
+            if (ballPoint.X + 100 >= ClientRectangle.Right)
+            {
+                speedX = -Math.Abs(speedX);
+            }
+            if (ballPoint.X <= 0)
+            {
+                speedX = Math.Abs(speedX);
+            }
+
+            if (vesselPoint.X <= 0)
+            {
+                vesselPoint.X = 0;
+            }
+            if (vesselPoint.X + 300 >= ClientRectangle.Right)
+            {
+                vesselPoint.X = ClientRectangle.Right - 300;
+            }
+
+            ballPoint.X += speedX;
+            ballPoint.Y += speedY;
+
+            labelCount.Text = "Отбиваний: " + count;
+            labelScore.Text = "Очков: " + score;
+            Refresh();
+
+            if (ballPoint.Y + 100 >= ClientRectangle.Bottom)
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            Cursor.Show();
+            string str = string.Format("Игра окончена!\nОтбиваний: {1}\nОчков: {0}", score, count);
+            DialogResult result = MessageBox.Show(str, "Game over");
+            if (result == DialogResult.OK)
+            {
+                ballPoint = new Point(rand.Next(ClientSize.Width - 100), -100);
+                vesselPoint.X = ClientSize.Width / 2 - 150;
+                vesselPoint.Y = ClientSize.Height - 50;
+                speedX = rand.Next(4, 5) == 4 ? -5 : 5;
+                count = 0;
+                score = 0;
+                paused = true;
+                labelStart.Visible = true;
+                Refresh();
+            }
+        }
+    }
+}
